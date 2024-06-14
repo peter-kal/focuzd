dd77d77547e8c7b4fa11411056f0e58a43dc7d94
peter-kal <koiliaris.peter@gmail.com>
2024-06-14 19:39:26 +0300

    added focuzd_pomodoro_api

---
+++ packages/focuzd_pomodoro_api/bin/focuzd_pomodoro_api.dart
@@ -0,0 +1,5 @@
+import 'package:focuzd_pomodoro_api/focuzd_pomodoro_api.dart' as focuzd_pomodoro_api;
+
+void main(List<String> arguments) {
+  print('Hello world: ${focuzd_pomodoro_api.calculate()}!');
+}
---
+++ packages/focuzd_pomodoro_api/test/focuzd_pomodoro_api_test.dart
@@ -0,0 +1,8 @@
+import 'package:focuzd_pomodoro_api/focuzd_pomodoro_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
