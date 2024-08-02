a80da8126e69f9c1a671136204eb9295a690843b
peter-kal <koiliaris.peter@gmail.com>
2024-08-02 11:40:13 +0300

    put the switch in a bottom app bar

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_spinbox/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:focuzd/blocs/blocs.dart';
@@ -17,11 +16,24 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocBuilder<RepoBloc, RepoState>(
-        builder: (context, state) {
-          if (state is RepoVariablesGivenState) {
-            return ListView(
+    return BlocBuilder<RepoBloc, RepoState>(
+      builder: (context, state) {
+        if (state is RepoVariablesGivenState) {
+          return Scaffold(
+            bottomNavigationBar: BottomAppBar(
+              height: 95,
+              child: YaruSwitchListTile(
+                  value: state.windowOnTop,
+                  title: const Text("Window Always On Top"),
+                  subtitle: const Text(
+                      "If ticked the window will always be on top of other windows"),
+                  onChanged: (newValue) {
+                    BlocProvider.of<RepoBloc>(context).add(
+                        UpdateSettingVariables(
+                            selectedToChange: 1, changedVar: newValue));
+                  }),
+            ),
+            body: ListView(
               children: [
                 SettingsCardSpinBox(
                     kind: "min",
@@ -43,47 +55,43 @@ class _SettingsPageState extends State<SettingsPage> {
                     label: "Rounds",
                     changeable: 2,
                     value: state.requestedNumberOfSessions),
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: OutlinedButton(
+                      onPressed: () {
+                        BlocProvider.of<RepoBloc>(context).add(ResetSettings());
+                      },
+                      child: const Row(
+                        children: [
+                          Icon(Icons.restart_alt),
+                          Text("Reset to default")
+                        ],
+                      )),
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
                 const Divider(),
                 SizedBox(
                   height: MediaQuery.of(context).size.height / 40,
                 ),
-                YaruSwitchListTile(
-                    value: state.windowOnTop,
-                    title: const Text("Window Always On Top"),
-                    subtitle: const Text(
-                        "If ticked the window will always be on top of other windows"),
-                    onChanged: (newValue) {
-                      BlocProvider.of<RepoBloc>(context).add(
-                          UpdateSettingVariables(
-                              selectedToChange: 1, changedVar: newValue));
-                    }),
-                OutlinedButton(
-                    onPressed: () {
-                      BlocProvider.of<RepoBloc>(context).add(ResetSettings());
-                    },
-                    child: const Row(
-                      children: [
-                        Icon(Icons.restart_alt),
-                        Text("Reset to default")
-                      ],
-                    ))
               ],
-            );
-          } else {
-            return const CircularProgressIndicator();
-          }
-        },
-      ),
-      appBar: YaruWindowTitleBar(
-        title: Text(AppLocalizations.of(context)!.settingsPage),
-        leading: YaruIconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            BlocProvider.of<PageNavigationBloc>(context)
-                .add(const MainPageEvent());
-          },
-        ),
-      ),
-    );
+            ),
+            appBar: YaruWindowTitleBar(
+              title: Text(AppLocalizations.of(context)!.settingsPage),
+              leading: YaruIconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () {
+                  BlocProvider.of<PageNavigationBloc>(context)
+                      .add(const MainPageEvent());
+                },
+              ),
+            ),
+          );
+        } else {
+          return const YaruCircularProgressIndicator();
+        }
+      },
+    ); //sca
   }
 }
