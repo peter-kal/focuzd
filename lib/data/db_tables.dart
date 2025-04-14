bbd16809e988f8d26f78a6e9eb150f876c52f670
Peter Koiliaris <koiliaris.peter@gmail.com>
2025-04-14 14:16:01 +0200

    oops forgot

--- lib/data/db_tables.dart
+++ lib/data/db_tables.dart
@@ -14,7 +14,7 @@ class MemorySessionVariable extends Table {
       boolean().withDefault(const Constant(false)).nullable()();
 
   TextColumn get type => text()(); //'work' 'break' 'longbreak'
-  TextColumn get subject => text().nullable()();
+  IntColumn get subject => integer().nullable()();
 }
 
 class SettingsVariables extends Table {
@@ -24,4 +24,16 @@ class SettingsVariables extends Table {
   IntColumn get selectedBreakDurationStored => integer()();
   IntColumn get selectedWorkDurationStored => integer()();
   IntColumn get selectedLongBreakDurationStored => integer()();
+}
+
+class Subject extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get name => text().unique().withLength(max: 100)();
+  IntColumn get subjectid =>
+      integer().nullable().customConstraint('REFERENCES subject(id)')();
+  DateTimeColumn get createdAt => dateTime()();
+  DateTimeColumn get updatedAt => dateTime()();
+  IntColumn get lastWorkedOnSessionID =>
+      integer().references(MemorySessionVariable, #id)();
+  TextColumn get notes => text().nullable()();
 }
