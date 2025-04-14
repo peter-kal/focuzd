ebfe1b2465d40ef06c2725d0739ee250b8535c06
Peter Koiliaris <koiliaris.peter@gmail.com>
2025-04-14 14:17:11 +0200

    updated the session card, will change later on

--- lib/extra_widgets/session_card.dart
+++ lib/extra_widgets/session_card.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:focuzd/blocs/repo_bloc/repo_bloc.dart';
+import 'package:focuzd/data/app_db.dart';
+import 'package:focuzd/data/repo.dart';
+import 'package:path/path.dart';
+import 'package:yaru/widgets.dart';
 
 class SessionCard extends StatelessWidget {
-  const SessionCard(this.id, {super.key});
-  final id;
+  SessionCard(this.id, {super.key});
+  MemorySessionVariableData id;
+  var memoryrepo = MemorySessionRepository(AppDatabase.instance);
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Text(id.toString()),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+              "${id.finishTime!.day.toString()}/${id.finishTime!.month.toString()}/${id.finishTime!.year.toString()}"),
+          Text(style: TextStyle(color: Colors.red), id.type),
+          Text(style: TextStyle(color: Colors.green), id.subject.toString()),
+          YaruIconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () {
+              memoryrepo.deleteMemorySessionById(id.id);
+              BlocProvider.of<RepoBloc>(context).add(EmitStateWithDBVars());
+            },
+          ),
+        ],
+      ),
     );
   }
 }
