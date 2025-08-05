89aead8e488e452afb6da2c68d4222577be3e723
peter-kal <koiliaris.peter@gmail.com>
2025-08-05 23:30:50 +0300

    improved SessionCard on the issue of subject, temp

--- lib/extra_widgets/session_card.dart
+++ lib/extra_widgets/session_card.dart
@@ -18,28 +18,45 @@ class SessionCard extends StatelessWidget {
       return n!.name;
     }
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text(
-              "${id.finishTime!.day.toString()}/${id.finishTime!.month.toString()}/${id.finishTime!.year.toString()}"),
-          Text(style: TextStyle(color: Colors.red), id.type),
-          Text(
-              style: TextStyle(color: Colors.green),
-              getSubjectName(id.subject!).toString()),
-          YaruIconButton(
-            icon: Icon(Icons.delete),
-            onPressed: () {
-              memoryrepo.deleteMemorySessionById(id.id);
-              BlocProvider.of<RepoBloc>(context).add(EmitStateWithDBVars());
+@override
+Widget build(BuildContext context) {
+  return Card(
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Text(
+            "${id.finishTime!.day.toString()}/${id.finishTime!.month.toString()}/${id.finishTime!.year.toString()}"),
+        Text(style: TextStyle(color: Colors.red), id.type),
+        if (id.type == 'break' || id.type == 'longBreak')
+          const SizedBox.shrink()
+        else
+          FutureBuilder<String?>(
+            future: getSubjectName(id.subject ?? -1),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Text("...");
+              } else if (snapshot.hasData && snapshot.data != null) {
+                return Text(
+                  snapshot.data!,
+                  style: const TextStyle(color: Colors.green),
+                );
+              } else {
+                return const Text(
+                  "No subject",
+                  style: TextStyle(color: Colors.green),
+                );
+              }
             },
           ),
-        ],
-      ),
-    );
-  }
+        YaruIconButton(
+          icon: Icon(Icons.delete),
+          onPressed: () {
+            memoryrepo.deleteMemorySessionById(id.id);
+            BlocProvider.of<RepoBloc>(context).add(EmitStateWithDBVars());
+          },
+        ),
+      ],
+    ),
+  );
+}
 }
