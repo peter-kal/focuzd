44abcccb4ae2106d5934264fb5b981c1106ca6bb
peter-kal <koiliaris.peter@gmail.com>
2025-08-29 17:39:40 +0300

    oops: forgot to add files to prev commit

---
+++ lib/pages/subject_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:focuzd/blocs/blocs.dart';
+import 'package:focuzd/data/app_db.dart';
+import 'package:yaru/yaru.dart';
+import 'package:focuzd/l10n/app_localizations.dart' as l10n;
+
+class IndividualSubjectPage extends StatelessWidget {
+  const IndividualSubjectPage({super.key, required this.subject});
+  final SubjectData subject;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text(subject.address),
+      floatingActionButton:
+          BlocBuilder<PageNavigationBloc, PageNavigationState>(
+              builder: (context, state) {
+        if (state is SubjectPageState) {
+          return FloatingActionButton(
+              child: Icon(Icons.delete_rounded),
+              onPressed: () {
+                BlocProvider.of<RepoBloc>(context)
+                    .add(DeleteSubjectDB(id: state.subject.id));
+                BlocProvider.of<PageNavigationBloc>(context)
+                    .add(SubjectsPageEvent());
+              });
+        } else {
+          return FloatingActionButton(
+              child: Icon(Icons.delete_forever), onPressed: () {});
+        }
+      }),
+      appBar: YaruWindowTitleBar(
+        title: Text(subject!.name),
+        leading: YaruIconButton(
+          tooltip: l10n.AppLocalizations.of(context)!.backArrowTooltip,
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            BlocProvider.of<PageNavigationBloc>(context)
+                .add(const SubjectsPageEvent());
+          },
+        ),
+      ),
+    );
+
+    //sca
+  }
+}
