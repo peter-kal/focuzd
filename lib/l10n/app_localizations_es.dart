e043a666197d1ca99734ca4881c345f45a86fbf5
peter-kal <koiliaris.peter@gmail.com>
2026-08-05 14:42:47 +0300

    Updated app_localizations_es

--- lib/l10n/app_localizations_es.dart
+++ lib/l10n/app_localizations_es.dart
@@ -9,7 +9,7 @@ class AppLocalizationsEs extends AppLocalizations {
   AppLocalizationsEs([String locale = 'es']) : super(locale);
 
   @override
-  String get mainPage => 'Main Page';
+  String get mainPage => 'Página principal';
 
   @override
   String get settingsPage => 'Ajustes';
