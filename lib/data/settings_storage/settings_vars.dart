ca9112be5d51c47b1feb96a51582b4e7f7cc267e
peter-kal <koiliaris.peter@gmail.com>
2026-08-06 14:05:39 +0300

    format code removed unnecessary import

--- lib/data/settings_storage/settings_vars.dart
+++ lib/data/settings_storage/settings_vars.dart
@@ -1,15 +1,18 @@
 import 'dart:core';
 import 'package:drift/drift.dart';
-import 'package:focuzd/data/settings_storage/db_settings.dart';
-
 
 class SettingsVariables extends Table {
   IntColumn get id => integer().autoIncrement()();
   BoolColumn get windowOnTop => boolean().withDefault(const Constant(false))();
-  IntColumn get defaultNumberOfSessionsPerRound => integer().withDefault(const Constant(4))();
-  IntColumn get defaultBreakDurationStored => integer().withDefault(const Constant(5))();
-  IntColumn get defaultFocusDurationStored => integer().withDefault(const Constant(25))();
-  IntColumn get defaultLongBreakDurationStored => integer().withDefault(const Constant(15))();
+  IntColumn get defaultNumberOfSessionsPerRound =>
+      integer().withDefault(const Constant(4))();
+  IntColumn get defaultBreakDurationStored =>
+      integer().withDefault(const Constant(5))();
+  IntColumn get defaultFocusDurationStored =>
+      integer().withDefault(const Constant(25))();
+  IntColumn get defaultLongBreakDurationStored =>
+      integer().withDefault(const Constant(15))();
   BoolColumn get atWillStart => boolean().withDefault(const Constant(false))();
-  IntColumn get periodofLongBreak => integer().withDefault(const Constant(4))(); // how many sessions to complete before a long break
+  IntColumn get periodofLongBreak => integer().withDefault(
+      const Constant(4))(); // how many sessions to complete before a long break
 }
